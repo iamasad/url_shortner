64c9183f7d8bbe61c1fb17db94bd40ef970d369b
Asad <asad_76@live.com>
2021-09-07 15:43:29 +0530

    first commit

---
+++ shortner/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import uuid
+from django.http import HttpResponse
+from .models import url
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def create(request):
+    if request.method =='POST':
+        link= request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = url(link=link,uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
